62566c60f652cb0b601be061960f1fba19881ec1
Zoltan Kochan <z@kochan.io>
2018-11-30 22:01:25 +0200

    test: make test pass

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,11 @@
 import dint = require('dint')
-import loadJsonFile = require('load-json-file')
+import loadJsonFile from 'load-json-file'
 import path = require('path')
 
 export default async function untouched (pkgDir: string): Promise<false | {}> {
   let dirIntegrity: {} | null = null
   try {
-    dirIntegrity = await loadJsonFile(path.join(path.dirname(pkgDir), 'integrity.json'))
+    dirIntegrity = await loadJsonFile<{}>(path.join(path.dirname(pkgDir), 'integrity.json'))
   } catch (err) {
     if (err.code !== 'ENOENT') throw err
     return false // for backward compatibility
